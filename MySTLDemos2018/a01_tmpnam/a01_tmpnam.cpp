5643f3fe965110e26edd481fac354b47f6c5c9a5
zcatt <zcatt@163.com>
2018-04-16 22:58:06 +0800

    create, 180416

---
+++ MySTLDemos2018/a01_tmpnam/a01_tmpnam.cpp
@@ -0,0 +1,27 @@
+﻿/*
+
+	a01_tmpnam
+
+	tmpnam()可以产生临时文件名，最大长度L_tmpnam。TMP_MAX是其可产生的单一文件名最大数量.
+
+
+*/
+
+#include <iostream>
+#include <cstdio>
+
+void main()
+{
+	using namespace std;
+	cout << "Up to "<< TMP_MAX << " temporary name of up to "<<L_tmpnam <<" chars\n"; 
+
+	char sName[L_tmpnam] = {'\0'};
+	cout << "names: \n";
+	for(int i = 0; i<10; i++)
+	{
+		tmpnam(sName);
+		cout<<sName<<endl;
+	}
+	return;
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MyDemos2018
+This project contains some demos.
+
+The details of these demos are in [Note.txt](Note.txt).
+
+**Requests and Bug reports**
+
+- [GitHub issues](https://github.com/zcatt/MyDemos2018/issues)
+
+**Git the source**
+
+- [Git and GitHub](https://github.com/zcatt/MyDemos2018)
+
