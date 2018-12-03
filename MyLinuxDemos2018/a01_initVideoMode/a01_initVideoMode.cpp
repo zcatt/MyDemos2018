a125513592703d9bd9007fb544aec2e25ac0669e
zcatt <zcatt@163.com>
2018-12-03 22:40:01 +0800

    add a01_initVideoMode

---
+++ MyLinuxDemos2018/a01_initVideoMode/a01_initVideoMode.cpp
@@ -0,0 +1,31 @@
+﻿#include <SDL.h>
+#include <stdlib.h>
+int main()
+{
+    SDL_Surface *screen;
+    Uint32 color;
+    int x;
+    if(SDL_Init(SDL_INIT_VIDEO)<0)
+    {
+        fprintf(stderr, "cannot init SDL");
+        exit(1);
+    }
+    screen = SDL_SetVideoMode(640, 480, 16, SDL_SWSURFACE);
+    if(screen == NULL)
+    {
+        fprintf(stderr, "cannot set video mode ");
+        exit(1);
+    }
+
+    atexit(SDL_Quit);
+
+    for(x = 0; x<=255; x++)
+    {
+        color = SDL_MapRGB(screen->format, 255, x,x);
+        SDL_FillRect(screen, NULL, color);
+        SDL_UpdateRect(screen, 0,0,0,0);
+        SDL_Delay(5);
+    }
+    SDL_Delay(3000);
+    return 0;
+}
